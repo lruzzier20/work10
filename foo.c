193e22b2b1a01ac3ce3a3ad0a92f42a98745ceef
Logan Ruzzier <lruzzier@DESKTOP-MQGMU3Q.localdomain>
2021-10-18 23:17:39 -0400

    edited homework

--- foo.c
+++ foo.c
@@ -8,15 +8,17 @@ struct note {
   struct note* next;
 };
 void structprnt(struct note *f){
-  printf("Timbre=%s\tHertz=%d\n",f->timbre,f->hertz);
+  printf("Timbre=%s, Hertz=%d\t",f->timbre,f->hertz);
 }
 void print_list(struct note *g){
-  struct note* temp=g;
+  if(g){struct note* temp=g;
+    printf("[");
   while(temp->next){
     structprnt(temp);
     temp=temp->next;
   }
   structprnt(temp);
+  printf("]\n");}else{printf("[]\n");}
 }
 struct note* insert_front(struct note* top, char* t, int hz){
   struct note* s=malloc(sizeof(struct note));
@@ -36,4 +38,23 @@ struct note* free_list(struct note* start){
   }
   free(t);
   return t3;
+}
+struct note * remove_node(struct note *front, int data){
+  struct note* back=NULL;
+  struct note* t=front;
+  struct note* ret=front;
+  while(t){
+    if(t->hertz!=data){
+      back=t;
+      t=t->next;
+    }else{
+      if(back!=NULL){
+        back->next=t->next;
+      }else{
+        ret=t->next;
+      }
+      t=t->next;}
+  }
+
+  return ret;
 }
